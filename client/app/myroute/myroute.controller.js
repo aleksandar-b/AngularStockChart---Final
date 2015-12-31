6694490d7673bc9593d96fc87f35fc4bbb639e59
samosale <slobodaodmah@gmail.com>
2015-12-31 20:53:26 +0100

    audio

--- client/app/myroute/myroute.controller.js
+++ client/app/myroute/myroute.controller.js
@@ -3,4 +3,5 @@
 angular.module('hApp')
   .controller('MyrouteCtrl', function ($scope) {
     $scope.message = 'Hello';
+    $scope.isActive = true;
   });
--- client/components/highCharts/highCharts.directive.js
+++ client/components/highCharts/highCharts.directive.js
@@ -6,9 +6,41 @@ angular.module('hApp')
       templateUrl: 'components/highCharts/highCharts.html',
       restrict: 'E',
       link: function (scope, element, attrs) {
+        console.log(scope);
+        console.log(element);
         
+        element[0].children[0].onclick = function(){
+          element.addClass('klas');
+          element[0].children[0].children[0].children[0].children[1].children[0].children[17].style.fill = 'white';
+     
+      var t = element[0].children[0].children[0].children[0].children[1].children[0].children[18].children;
+      for(var i = 0; i<7; i++){
+        
+        t[i].style.fill = 'white';
+      }
+      console.log(element);
+     
+     
+        }
+     /*   
+       // var t = Array.from(element[0].children[0].children[0].children[0].children[1].children[0].children[17].children);
+     // console.log(t);
+     */
    
-        
+   
+   
+   
+   
+ /*
+ Array.from(element[0].children[0].children[0].children[0].children[1].children[0].children[18].children).forEach(function(val){       
+   
+       val.style.fill = 'white';
+    
+       
+     // element[0].children[0].children[0].children[0].children[1].children[0].children[18][0].style.fill = 'white';
+     
+      });
+      */
       },
     controller: 'highChartsController',
   
